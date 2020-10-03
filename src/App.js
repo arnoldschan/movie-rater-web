ad472f5db330add87893c0d9de92cd2ee8a4771c
Cloud User <centos@ip-172-31-27-80.ap-northeast-1.compute.internal>
2020-10-03 17:01:30 +0800

    feat: display rating

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
 import './App.css';
+import MovieList from './components/MovieList'
+import MovieDetail from './components/MovieDetail'
 
 function App() {
+  
+  const [movies, setMovies] = useState(['Movie 1','Movie 2']);
+  const [selectedMovie, setSelectedMovie] = useState(null);
+
+  useEffect(()=> {
+    fetch("http://djangoproject--arnoldschan.repl.co/api/movies/", {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Token 12f17f8d7daa4f40a6155138e9894bfecccb1598',
+      }
+    })
+    .then( resp => resp.json())
+    .then( resp => setMovies(resp))
+    .catch( error => console.log(error))
+  }, [])
+  const movieClicked = movie => {
+    setSelectedMovie(movie);
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Movie Rater</h1>
+        <div className="layout">
+          <MovieList movies={movies} movieClicked={movieClicked}/>
+          <MovieDetail movie={selectedMovie}/>
+          <div>Movie details</div>
+        </div>
       </header>
     </div>
   );
 }
+
 
 export default App;
